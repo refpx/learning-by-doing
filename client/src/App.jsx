b955126fd3825d0f99077c4eb117351e51703c18
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-08-18 08:26:41 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>App Go</h1>
+      <button
+        onClick={async () => {
+          const response = await fetch('http://localhost:3000/users')
+          const data = await response.json()
+          console.log(data)
+        }}>
+        Obtener datos
+      </button>
+    </div>
+  )
+}
+
+export default App
