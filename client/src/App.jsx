e2eb1739a79ccd15116b0ee63f2d5d1c4b3c56b0
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-08-18 09:57:52 -0500

    First commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,47 @@
+import { useEffect, useState } from 'react'
+
 function App() {
+  const [name, setName] = useState('second')
+  const [users, setUsers] = useState([])
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const response = await fetch(import.meta.env.VITE_API + '/users', {
+      method: 'POST',
+      body: JSON.stringify({ name }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+    console.log(data)
+    loadUsers()
+  }
+
+  const loadUsers = async () => {
+    const response = await fetch(import.meta.env.VITE_API + '/users')
+    const { data } = await response.json()
+    setUsers(data)
+  }
+  useEffect(() => {
+    loadUsers()
+  }, [])
+
   return (
     <div>
-      <h1>App Go</h1>
-      <button
-        onClick={async () => {
-          const response = await fetch('http://localhost:3000/users')
-          const data = await response.json()
-          console.log(data)
-        }}>
-        Obtener datos
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Set your name'
+          onChange={e => setName(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
