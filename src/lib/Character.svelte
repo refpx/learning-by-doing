9674ed6489bc233178fb5082b07492b513a1ca3e
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-08-25 23:19:52 -0500

    First commit

---
+++ src/lib/Character.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let character
+</script>
+
+<div class="character">
+  <img src={character.image} alt={character.name} />
+  <h2>{character.name}</h2>
+  <p>{character.species}</p>
+</div>
