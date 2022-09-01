530c69a8667dece003ea994ef8a446b64c64c256
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-09-01 18:46:49 -0500

    2. Variables, constants and zero values

--- 1. Basics/src/main.go
+++ 1. Basics/src/main.go
@@ -1,11 +1,48 @@
 // 1. Hello world
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
+
+// ================================================
+
+// 2. Variables, constants and zero values
+
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Declaración de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Declaración de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Calcular el área de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("El área del cuadrado es:", areaCuadrado)
 }
 
 // ================================================
