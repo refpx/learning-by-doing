bd38571bc165dc244652902af7c6370424197b55
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-09-07 16:06:37 -0500

    Docker File para Producción

--- server/server.go
+++ server/server.go
@@ -10,6 +10,7 @@ import (
 	database "github.com/refpx/go-rest-ws/database"
 	repository "github.com/refpx/go-rest-ws/repository"
 	websocket "github.com/refpx/go-rest-ws/websocket"
+	"github.com/rs/cors"
 )
 
 type Config struct {
@@ -62,6 +63,7 @@ func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
 	b.router = mux.NewRouter()
 	binder(b, b.router)
+	handler := cors.Default().Handler(b.router)
 	repo, err := database.NewPostgresRepository(b.config.DatabaseUrl)
 	if err != nil {
 		log.Fatal(err)
@@ -69,7 +71,7 @@ func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
 	go b.hub.Run()
 	repository.SetRepository(repo)
 	log.Println("Starting server on port", b.Config().Port)
-	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+	if err := http.ListenAndServe(b.config.Port, handler); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
