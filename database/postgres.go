3ad8ac685fb5f6fed76f69533abfb7824d42c03c
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-09-07 10:51:14 -0500

    Paginación para Posts

--- database/postgres.go
+++ database/postgres.go
@@ -113,6 +113,32 @@ func (repo *PostgresRespository) DeletePost(ctx context.Context, id string, user
 	return err
 }
 
+func (repo *PostgresRespository) ListPost(ctx context.Context, page uint64) ([]*models.Post, error) {
+	rows, err := repo.db.QueryContext(ctx, "SELECT id, post_content, user_id, created_at FROM posts LIMIT $1 OFFSET $2", 2, page*2)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		err = rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	var posts []*models.Post
+	for rows.Next() {
+		var post = models.Post{}
+		if err := rows.Scan(&post.Id, &post.PostContent, &post.UserId, &post.CreatedAt); err == nil {
+			posts = append(posts, &post)
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return posts, nil
+}
+
 func (repo *PostgresRespository) Close() error {
 	return repo.db.Close()
 }
