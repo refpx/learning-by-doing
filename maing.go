830e2a20099cde176c92aed31cd17b7ce3cf1b3b
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-09-08 11:01:17 -0500

    Creación de tablas

--- maing.go
+++ maing.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/refpx/go-gorm-rest-api/db"
+	"github.com/refpx/go-gorm-rest-api/models"
 	"github.com/refpx/go-gorm-rest-api/routes"
 )
 
 func main() {
 	db.DBConnection()
+	db.DB.AutoMigrate(models.User{}, models.Task{})
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
 
