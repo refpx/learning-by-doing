3e2c7ca0d6d4deded773f8019a7f4d79d248f0b2
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-09-09 11:33:50 -0500

    Setup go-fiber and react-vite

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button
+        onClick={async () => {
+          const response = await fetch('/users')
+          const data = await response.json()
+          console.log(data)
+        }}>
+        Obtener datos
+      </button>
+    </div>
+  )
+}
+
+export default App
