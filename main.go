e7fa8ed44e7239450d52cfafa6531ca9d48ae9e8
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-09-13 00:29:29 -0500

    Implementations

--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/refpx/url-shortener/handler"
+	"github.com/refpx/url-shortener/store"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hey Go URL Shortener! ⚡",
+			"message": "Hey Go URL Shortener API! ⚡",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
