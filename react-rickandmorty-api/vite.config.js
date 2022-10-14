83cf40cc8a8589258a23b8f32527d5d4828aad23
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-10-14 11:04:53 -0500

    feat: update branch

--- react-rickandmorty-api/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'https://refpx.github.io/react-rick-and-morty',
-  plugins: [react()],
-})
