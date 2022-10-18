e6ac1f3046ba3ab25a53047f72db429e92aa5d0c
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-10-18 15:40:20 -0500

    update project

--- rest-api-crud/src/routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-const router = Router()
-
-router.get('/', (req, res) => res.send('Hello World!'))
-router.get('/p', (req, res) => res.send('Piong!'))
-
-export default router
