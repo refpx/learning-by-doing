7dd476804464764181eb98d55a534abadf852d58
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-10-18 14:58:22 -0500

    feat: update project

--- CODE/go-course/go-poo/modules/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/refpx/test
-
-go 1.19
-
-require (
-	github.com/donvito/hellomod v1.0.1
-	github.com/donvito/hellomod/v2 v2.0.0
-)
--- CODE/go-rest-ws/models/claims.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "github.com/golang-jwt/jwt"
-
-type AppClaims struct {
-	UserId string `json:"user_id"`
-	jwt.StandardClaims
-}
