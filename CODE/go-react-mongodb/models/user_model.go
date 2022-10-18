7d64682a6ed9aace7e61d1339db188c76a45ef19
Fredy Romero <romeroespinoza.fp@gmail.com>
2022-10-18 14:51:32 -0500

    feat: project update

--- CODE/go-react-mongodb/models/user_model.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type User struct {
-	Id   primitive.ObjectID `json:"_id" bson:"_id"`
-	Name string             `json:"name"`
-}
