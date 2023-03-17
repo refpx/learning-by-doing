1a2e7c9c5217db691d3239874665177a1e846629
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-03-17 17:09:36 -0500

    TODO: test in App component

---
+++ tests/App.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import App from '../src/App'
+
+describe('Test en <App />', () => {
+  test('todo: define a test', () => {
+    render(<App />)
+    // screen.debug()
+  })
+})
