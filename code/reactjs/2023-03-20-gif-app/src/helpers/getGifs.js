01772624f8bde36ef8ff4391e370caa83ecc15d3
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-03-21 00:27:50 -0500

    fix: use .env to api_key

--- code/reactjs/2023-03-20-gif-app/src/helpers/getGifs.js
+++ code/reactjs/2023-03-20-gif-app/src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const URL = `https://api.giphy.com/v1/gifs/search?api_key=afzufYoS5L9nB0g6m0tJrkUbPkDHB7C2&q=${category}&limit=10`
+  const URL = `https://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_API_KEY ?? process.env.API_KEY}&q=${category}&limit=10`
 
   const response = await fetch(URL)
   const { data } = await response.json()
