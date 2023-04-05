22d7e20c2255c50607b82450bbc78d3cf2208a98
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-04-05 16:13:42 -0500

    JournalApp: done registar page design

--- code/react/2023-05-04-journal-app/src/auth/pages/RegisterPage.jsx
+++ code/react/2023-05-04-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,51 @@
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+import { AuthLayout } from '../layout/AuthLayout'
+
 export function RegisterPage () {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Register'>
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Full Name'
+              type='text'
+              placeholder='Fredy Romero'
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Email'
+              type='email'
+              placeholder='email@email.com'
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Password'
+              type='password'
+              placeholder='********'
+              fullWidth
+            />
+          </Grid>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant='contained' fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+            <Link component={RouterLink} color='inherit' to='/auth/login'>
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
