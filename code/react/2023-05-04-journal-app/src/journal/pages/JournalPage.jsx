046d958118525828c0aa7db5916f46bfa29f9b65
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-04-05 17:19:50 -0500

    JournalApp: done project design

--- code/react/2023-05-04-journal-app/src/journal/pages/JournalPage.jsx
+++ code/react/2023-05-04-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,33 @@
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
+import { AddOutlined } from '@mui/icons-material'
 
 export function JournalPage () {
   return (
     <JournalLayout>
       {/* <NothingSelectedView /> */}
       <NoteView />
+
+      <IconButton
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {
+            backgroundColor: 'error.main',
+            opacity: 0.9
+          },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined
+          sx={{
+            fontSize: 30
+          }}
+        />
+      </IconButton>
     </JournalLayout>
   )
 }
