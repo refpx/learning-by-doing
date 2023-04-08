5b66573d14ad796fc80c34ba0f9d58e356b429da
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-04-08 17:13:26 -0500

    JournalApp: setup slices and thunks

--- code/react/2023-05-04-journal-app/src/auth/pages/LoginPage.jsx
+++ code/react/2023-05-04-journal-app/src/auth/pages/LoginPage.jsx
@@ -2,11 +2,33 @@ import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import { Link as RouterLink } from 'react-router-dom'
 import { Google } from '@mui/icons-material'
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
+import { useDispatch } from 'react-redux'
+import { checkingAuth, startGoogleSignIn } from '../../store/auth'
 
 export function LoginPage () {
+  const dispatch = useDispatch()
+
+  const { email, password, onInputChange } = useForm({
+    email: 'fredy@email.com',
+    password: '123456'
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(email, password)
+    dispatch(checkingAuth())
+  }
+
+  const onGoogleSignIn = () => {
+    console.log('On Google Sign In')
+    dispatch(startGoogleSignIn())
+  }
+
   return (
     <AuthLayout title='Sign in'>
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +36,9 @@ export function LoginPage () {
               type='email'
               placeholder='email@email.com'
               fullWidth
+              name='email'
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -22,16 +47,19 @@ export function LoginPage () {
               type='password'
               placeholder='********'
               fullWidth
+              name='password'
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth>
+              <Button variant='contained' fullWidth type='submit'>
                 Sign in
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth>
+              <Button variant='contained' fullWidth onClick={onGoogleSignIn}>
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
---
+++ code/react/2023-05-04-journal-app/src/store/auth/thunks.js
@@ -0,0 +1,13 @@
+import { checkingCredentials } from './'
+
+export const checkingAuth = (email, password) => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials())
+  }
+}
+
+export const startGoogleSignIn = () => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials())
+  }
+}
