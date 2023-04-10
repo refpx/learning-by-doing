7337cc29086bfb7c33942b218aec006599f4987b
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-04-10 17:32:39 -0500

    JournalApp: done useCheckAuth

--- code/react/2023-05-04-journal-app/src/router/AppRouter.jsx
+++ code/react/2023-05-04-journal-app/src/router/AppRouter.jsx
@@ -1,26 +1,11 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
-import { useDispatch, useSelector } from 'react-redux'
 import { CheckingAuth } from '../ui/components/CheckingAuth'
-import { useEffect } from 'react'
-import { onAuthStateChanged } from 'firebase/auth'
-import { FirebaseAuth } from '../firebase/config'
-import { login, logout } from '../store/auth'
+import { useCheckAuth } from '../hooks'
 
 export function AppRouter () {
-  const { status } = useSelector(state => state.auth)
-
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout())
-      const { uid, email, displayName, photoURL } = user
-      dispatch(login({ uid, email, displayName, photoURL }))
-    })
-  }, [])
-
+  const status = useCheckAuth()
   if (status === 'checking') return <CheckingAuth />
 
   return (
