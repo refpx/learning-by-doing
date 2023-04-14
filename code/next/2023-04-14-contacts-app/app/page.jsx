b539b0abd09ed65b5a2de39d2893fe7ed1282319
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-04-14 10:06:13 -0500

    feat: fetch users

--- code/next/2023-04-14-contacts-app/app/page.jsx
+++ code/next/2023-04-14-contacts-app/app/page.jsx
@@ -1,7 +1,23 @@
-function Home() {
+const fetchUsers = async () => { 
+  const res = await fetch('https://reqres.in/api/users')
+  const data = await res.json()
+  return data.data
+}
+
+async function Home() {
+  const users = await fetchUsers()
+  console.log(users)
+
   return (
     <div>
       <h1>Home page</h1>
+      <hr />
+
+      <div>
+        {
+          JSON.stringify(users)
+        }
+      </div>
     </div>
   )
 }
