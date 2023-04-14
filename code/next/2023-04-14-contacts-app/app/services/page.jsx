455466eb5a85c0eb0cb66f02314fa6bb0912613b
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-04-14 09:50:56 -0500

    feat: create sample pages

---
+++ code/next/2023-04-14-contacts-app/app/services/page.jsx
@@ -0,0 +1,9 @@
+function Services() {
+  return (
+    <div>
+      <h1>Services page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed sit quod vel corrupti, dolore molestiae est. Vel qui quae et provident voluptate. Consequatur dolor odit laudantium rerum dolore dolorem eum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. At nostrum, minima fugiat nesciunt optio architecto quaerat reiciendis quidem ipsam voluptatibus. Dolores blanditiis tempora earum neque ipsam error reiciendis eius consectetur.</p>
+    </div>
+  )
+}
+export default Services
