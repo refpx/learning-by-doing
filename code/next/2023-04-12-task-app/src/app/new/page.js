a72997c178c3f1856b78cdec9442d2a1eee1920d
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-04-14 08:38:17 -0500

    feat: add react-hook-form and update form

--- code/next/2023-04-12-task-app/src/app/new/page.js
+++ code/next/2023-04-12-task-app/src/app/new/page.js
@@ -1,53 +1,55 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { useTasks } from '@/context/TasksContext'
+import { useForm } from 'react-hook-form'
 
 function Page({ params }) {
-  const [task, setTask] = useState({
-    title: '',
-    description: '',
-  })
   const { tasks, createTask, updateTask } = useTasks()
   const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm()
 
-  const handleChange = (e) => {
-    setTask({ ...task, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
+  const onSubmit = handleSubmit((data) => {
     if (params.id) {
-      updateTask(params.id, task)
+      updateTask(params.id, data)
     } else {
-      createTask(task.title, task.description)
+      createTask(data.title, data.description)
     }
     router.push('/')
-  }
+  })
 
   useEffect(() => {
     if (params.id) {
-      setTask(tasks.find((task) => task.id === parseInt(params.id)))
+      const taskFound = tasks.find((task) => task.id === parseInt(params.id))
+      if (taskFound) {
+        setValue('title', taskFound.title)
+        setValue('description', taskFound.description)
+      }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={onSubmit}>
       <input
         type='text'
-        name='title'
         placeholder='Some title'
-        value={task?.title}
-        onChange={handleChange}
+        {...register('title', { required: true })}
       />
+      {errors.title && <span>This field is required</span>}
+
       <textarea
-        name='description'
         placeholder='Some awesome description...'
-        value={task?.description}
-        onChange={handleChange}
+        {...register('description', { required: true })}
       />
+      {errors.description && <span>This field is required</span>}
+
       <button type='submit'>Save</button>
     </form>
   )
