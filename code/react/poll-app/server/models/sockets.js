b8a7f672421beba31fb2b404ad06f2ddb4ab3f7f
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-04-14 16:49:57 -0500

    feat: test sockets events

--- code/react/poll-app/server/models/sockets.js
+++ code/react/poll-app/server/models/sockets.js
@@ -1,12 +1,20 @@
+import BandList from './band-list.js'
+
 class Sockets {
   constructor(io) {
     this.io = io
+    this.bandList = new BandList()
     this.socketEvents()
   }
 
   socketEvents() {
     // On connection
-    // this.io.on('connection', (socket) => {})
+    this.io.on('connection', (socket) => {
+      console.log('Client connected', socket.id)
+
+      // Emit all bands when a client connects
+      socket.emit('current-bands', this.bandList.getBands())
+    })
   }
 }
 
