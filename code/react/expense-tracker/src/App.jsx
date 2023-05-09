9a0690f0a88364ebcc1a1d2c288f1d8405982e52
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-05-09 16:00:59 -0500

    feat: add income and expense values

--- code/react/expense-tracker/src/App.jsx
+++ code/react/expense-tracker/src/App.jsx
@@ -1,5 +1,6 @@
 import { Balance } from './components/Balance'
 import { Header } from './components/Header'
+import { IncomeExpenses } from './components/IncomeExpenses'
 import { TransactionForm } from './components/transactions/TransactionForm'
 import { TransactionList } from './components/transactions/TransactionList'
 import { GlobalProvider } from './context/GlobalState'
@@ -8,11 +9,10 @@ function App () {
   return (
     <GlobalProvider>
       <Header />
+      <IncomeExpenses />
       <Balance />
       <TransactionForm />
       <TransactionList />
-      <div>Hello World</div>
-
     </GlobalProvider>
   )
 }
