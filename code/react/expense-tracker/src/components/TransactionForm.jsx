418aad0a057b65a5e961d5c71865a73e1bdb7dd4
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-05-09 15:29:41 -0500

    feat: Add transaction to Global state

--- code/react/expense-tracker/src/components/TransactionForm.jsx
+++ code/react/expense-tracker/src/components/TransactionForm.jsx
@@ -1,5 +1,39 @@
+import { useState } from 'react'
+import { useGlobalState } from '../context/GlobalState'
+
 export function TransactionForm () {
+  const { addTransaction } = useGlobalState()
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    addTransaction({
+      id: 1,
+      description,
+      amount
+    })
+  }
+
   return (
-    <div>TransactionForm</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='Enter a description...'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type='number'
+          step={0.01}
+          placeholder='00.00'
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button type='submit'>Add Transaction</button>
+      </form>
+    </div>
   )
 }
--- code/react/expense-tracker/src/context/GlobalState.jsx
+++ code/react/expense-tracker/src/context/GlobalState.jsx
@@ -18,8 +18,15 @@ export const useGlobalState = () => {
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
 
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    })
+  }
+
   return (
-    <Context.Provider value={{ transactions: state.transactions }}>
+    <Context.Provider value={{ transactions: state.transactions, addTransaction }}>
       {children}
     </Context.Provider>
   )
