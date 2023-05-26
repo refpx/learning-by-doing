ecd966eb930ea326b664c1d0088ba82390b45e6b
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-05-26 14:33:18 -0500

    feat: done home page and google oauth

--- code/next/share-prompts/app/page.jsx
+++ code/next/share-prompts/app/page.jsx
@@ -1,6 +1,19 @@
+import Feed from '@components/Feed'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share
+        <br className='max-md:hidden' />
+        <span className='orange_gradient'> AI-Power Prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Share promts is an open source project that allows you to discover, create and share AI-powered prompts.
+      </p>
+
+      <Feed />
+    </section>
   )
 }
 export default Home
