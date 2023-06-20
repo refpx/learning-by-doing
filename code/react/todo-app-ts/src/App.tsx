9cc0e4d927535a3c10f6d96faa255696da85ff0d
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-06-20 18:03:56 -0500

    feat: add tasks list view

--- code/react/todo-app-ts/src/App.tsx
+++ code/react/todo-app-ts/src/App.tsx
@@ -1,6 +1,31 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1 : lorem ipsum dolor sit amet',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'todo 2: consectetur adipiscing elit',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3: sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    completed: false
+  }
+]
+
 const App = (): JSX.Element => {
+  const [todos] = useState(mockTodos)
+
   return (
-    <h1>todo mvc</h1>
+    <div className='todoapp'>
+      <Todos todos={todos} />
+    </div>
   )
 }
 export default App
