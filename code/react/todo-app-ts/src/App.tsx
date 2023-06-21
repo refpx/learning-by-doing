f638774e8892cb9770f15b3463848cf610e62031
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-06-21 16:07:08 -0500

    feat: add new todo item

--- code/react/todo-app-ts/src/App.tsx
+++ code/react/todo-app-ts/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import { Todos } from './components/Todos'
-import { type FilterValue, type TodoId, type Todo as TodoType } from './types'
+import { type TodoTitle, type FilterValue, type TodoId, type Todo as TodoType } from './types'
 import { TODO_FILTERS } from './consts'
 import { Footer } from './components/Footer'
+import { Header } from './components/Header'
 
 const mockTodos = [
   {
@@ -62,8 +63,21 @@ const App = (): JSX.Element => {
     return todo
   })
 
+  const handleAddTodo = ({ title }: TodoTitle): void => {
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title,
+      completed: false
+    }
+
+    const newTodos = [...todos, newTodo]
+
+    setTodos(newTodos)
+  }
+
   return (
     <div className='todoapp'>
+      <Header onAddTodo={handleAddTodo} />
       <Todos
         onRemoveTodo={handleRemove}
         onToggleCompleteTodo={handleCompleted}
