e2bd092b4a09a5e7d72cb9b503273e3f34d1b9d1
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-06-21 15:07:04 -0500

    feat: toggle todo item status

--- code/react/todo-app-ts/src/components/Todo.tsx
+++ code/react/todo-app-ts/src/components/Todo.tsx
@@ -1,17 +1,25 @@
 import { type TodoId, type Todo as TodoType } from '../types'
 
 interface Props extends TodoType {
+  onToggleCompleteTodo: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
   onRemoveTodo: ({ id }: TodoId) => void
 }
 
-export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo }) => {
+export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompleteTodo }) => {
+  const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    onToggleCompleteTodo({
+      id,
+      completed: event.target.checked
+    })
+  }
+
   return (
     <div className="view">
       <input
         className="toggle"
         checked={completed}
         type="checkbox"
-        onChange={() => {}}
+        onChange={handleChangeCheckbox}
       />
       <label>{title}</label>
       <button
